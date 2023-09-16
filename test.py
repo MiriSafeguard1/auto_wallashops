2213abbe859147a7f76842b76e0228a81a9d7808
Amir Gez <111357168+AmirGgh@users.noreply.github.com>
2023-09-16 18:16:58 +0300

    autometion - login -> cat

--- test.py
+++ test.py
@@ -2,17 +2,41 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = Options()
 chrome_options.add_experimental_option('detach', True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
-driver.get("https://www.wallashops.co.il/login?rurl=8")
 password =  "mpn6jPDhZJ9R!"
 username =  "amirg.uni12@gmail.com"
-driver.find_element_by_class_name("mobile-login-btn f-button f-button--white f-button--full-width collapsed").click()
-# driver.find_element_by_id("loginEmail").send_keys(username)
-# driver.find_element_by_id("loginPassword").send_keys(password)
-# driver.find_element_css_selector("input[type=\"submit\"i]").click()
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+driver.get("https://www.wallashops.co.il/login?rurl=8")
+
+# login options button
+button = driver.find_element(By.CLASS_NAME, "mobile-login-btn")
+if button.get_attribute("aria-expanded") == "false":
+    #change state & toggle to true
+    button.click()
+
+#insert username & password in the form
+driver.find_element(By.NAME, "loginEmail").send_keys(username)
+driver.find_element(By.NAME, "loginPassword").send_keys(password)
+
+# click on login button
+button = driver.find_element(By.XPATH, "//button[contains(text(), 'כניסה לוואלה!שופס')]")
+button.click()
+
+# click on some product
+wait = WebDriverWait(driver, 5)  # Wait for up to 10 seconds
+link = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "b-product-link")))
+driver.get(link.get_attribute("href"))
+
+# click on add to cart -> by clicking on the button getting into the cart
+wait = WebDriverWait(driver, 5)  # Wait for up to 10 seconds
+button = driver.find_element(By.CLASS_NAME, "js-add-to-cart")
+button.click() 
 
 print("login success")
